2291962c5eb24da1c52bf4e3d9a27be6388d94e3
sowmyatellapragada <sowmyatellapragada01@gmail.com>
2021-01-06 19:54:08 +0530

    Add project files.

---
+++ WpfApp2/WebsiteDataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class WebsiteDataModel
+    {
+        public string WebsiteUrl { get; set; } = "";
+        public string WebsiteData { get; set; } = "";
+    }
+}
